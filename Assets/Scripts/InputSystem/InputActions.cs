8b957b28617faaed49f3837fda62ddc31ab839da
StarSlash <genma23@hotmail.fr>
2020-02-01 11:41:40 +0100

    :sparkles: Getready to RUMBLE!!!

---
+++ Assets/Scripts/InputSystem/InputActions.cs
@@ -0,0 +1,164 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/InputSystem/InputActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputActions : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputActions()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputActions"",
+    ""maps"": [
+        {
+            ""name"": ""Movements"",
+            ""id"": ""c8591720-ea4c-4bae-b8e5-ff90e8aed879"",
+            ""actions"": [
+                {
+                    ""name"": ""Interact"",
+                    ""type"": ""Button"",
+                    ""id"": ""cf3c79b8-4a13-4096-b7d4-7c864ead6ba6"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""6e571658-791f-4c01-b77e-4cf96bad6948"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Interact"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""aa523da0-2820-43ea-a7b9-85336293e259"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Interact"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Gamepad"",
+            ""bindingGroup"": ""Gamepad"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Gamepad>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Movements
+        m_Movements = asset.FindActionMap("Movements", throwIfNotFound: true);
+        m_Movements_Interact = m_Movements.FindAction("Interact", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Movements
+    private readonly InputActionMap m_Movements;
+    private IMovementsActions m_MovementsActionsCallbackInterface;
+    private readonly InputAction m_Movements_Interact;
+    public struct MovementsActions
+    {
+        private @InputActions m_Wrapper;
+        public MovementsActions(@InputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Interact => m_Wrapper.m_Movements_Interact;
+        public InputActionMap Get() { return m_Wrapper.m_Movements; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(MovementsActions set) { return set.Get(); }
+        public void SetCallbacks(IMovementsActions instance)
+        {
+            if (m_Wrapper.m_MovementsActionsCallbackInterface != null)
+            {
+                @Interact.started -= m_Wrapper.m_MovementsActionsCallbackInterface.OnInteract;
+                @Interact.performed -= m_Wrapper.m_MovementsActionsCallbackInterface.OnInteract;
+                @Interact.canceled -= m_Wrapper.m_MovementsActionsCallbackInterface.OnInteract;
+            }
+            m_Wrapper.m_MovementsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Interact.started += instance.OnInteract;
+                @Interact.performed += instance.OnInteract;
+                @Interact.canceled += instance.OnInteract;
+            }
+        }
+    }
+    public MovementsActions @Movements => new MovementsActions(this);
+    private int m_GamepadSchemeIndex = -1;
+    public InputControlScheme GamepadScheme
+    {
+        get
+        {
+            if (m_GamepadSchemeIndex == -1) m_GamepadSchemeIndex = asset.FindControlSchemeIndex("Gamepad");
+            return asset.controlSchemes[m_GamepadSchemeIndex];
+        }
+    }
+    public interface IMovementsActions
+    {
+        void OnInteract(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    InputActions inputActions;
+    GamepadVibrate gpVibrate;
+
+    void Awake()
+    {
+        
+        gpVibrate = GetComponent<GamepadVibrate>();
+        inputActions = new InputActions();
+    }
+
+    public void VibrateRight()
+    {
+        gpVibrate.Vibrate(1.0f, 0.0f, 1.0f);
+        Invoke(nameof(VibrateLeft), 2.0f);
+    }
+    public void VibrateLeft()
+    {
+        gpVibrate.Vibrate(0.0f, 1.0f, 1.0f);
+        Invoke(nameof(VibrateRight), 2.0f);
+    }
+
+}
