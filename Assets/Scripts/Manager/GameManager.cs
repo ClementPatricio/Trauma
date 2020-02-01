2de3da2f4eb5bf65068c67ec22d003325fe5dd8b
dimit <dimit@DESKTOP-6AL8C74.local>
2020-02-01 19:19:10 +0100

    [ADD] Managers + bout de Shaders

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//phase de controle du systeme
+public enum GameState { intro, play, transition, souvenir , end };
+//phase de jeu
+public enum GameStage {accident,chat,cheminee,boite_a_musique,dessin,fleurs,photo }
+
+
+public class GameManager : MonoBehaviour
+{
+    GameState state;
+    GameStage stage;
+
+
+    //contient les différents manager
+    private LevelManager level_manager;
+    private UIManager ui_manager;
+    private SoundManager sound_manager;
+    private ControlerManager controler_manager;
+    private static GameManager instance;
+
+
+    public static GameManager getInstance()
+    {
+        //ou instantie si n'existe pas ?
+        return instance;
+    }
+
+    void Awake()
+    {
+        level_manager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+        ui_manager = GameObject.Find("UIManager").GetComponent<UIManager>();
+        sound_manager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+        controler_manager = GameObject.Find("ControlerManager").GetComponent<ControlerManager>();
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        setState(GameState.intro);
+    }
+
+    public void Stop()
+    {
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+                                Application.Quit();
+#endif
+    }
+
+    public void setStage(GameStage stage)
+    {
+        this.stage = stage;
+    }
+
+    public void setState(GameState state)
+    {
+        this.state = state;
+        level_manager.actualiseState(state);
+        ui_manager.actualiseState(state);
+        sound_manager.actualiseState(state);
+        controler_manager.actualiseState(state);
+
+    }
+
+    public GameState getState()
+    {
+        return this.state;
+    }
+    public GameStage getStage()
+    {
+        return this.stage;
+    }
+}
---
+++ Assets/Scripts/Render.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Render : MonoBehaviour
+{
+    public float intensity;
+    private Material mat;
+    int it = 0;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mat = new Material(Shader.Find("Shaders/ShaderTestDeux"));
+        //Debug.Log("Resolution "+Screen.currentResolution);
+        mat.SetFloat("height", Screen.currentResolution.height);
+        mat.SetFloat("width", Screen.currentResolution.width);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        /*if(intensity == 0)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+        mat.SetFloat("_bwBlend", intensity);*/
+        //Debug.Log("On Render Image");
+
+        //mat.SetFloat("time", Mathf.Sin(Time.realtimeSinceStartup));
+        mat.SetFloat("time", Mathf.Sin(Time.realtimeSinceStartup));
+        Graphics.Blit(source, destination, mat);
+    }
+
+    public void Update()
+    {
+        //mat.SetFloat("time", Mathf.Sin(Time.realtimeSinceStartup*40.0f));
+    }
+}
